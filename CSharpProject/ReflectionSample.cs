d7c4107e99017530b592b9e2adb49fb2ffd29fe2
mm <unam3d@gmx.de>
2021-10-10 00:08:30 +0200

    added reflection added sigil

---
+++ CSharpProject/ReflectionSample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace CSharpProject
+{
+    public class ReflectionSample
+    {
+
+        private static readonly PropertyInfo cachedSampeProperty = typeof(ClassWithPrivatePropertySample)
+            .GetProperty("PrivatePropery", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        private static readonly Func<ClassWithPrivatePropertySample, string> getPropertyDelegate =
+            (Func<ClassWithPrivatePropertySample, string>)Delegate.CreateDelegate(
+                typeof(Func<ClassWithPrivatePropertySample, string>),
+                cachedSampeProperty.GetGetMethod(true)!);
+
+        public void SamplePrivatePropCall()
+        {
+            var param = new ClassWithPrivatePropertySample();
+            var privatePropValue = getPropertyDelegate(param);
+        }
+       
+        public class ClassWithPrivatePropertySample
+        {
+            private string PrivatePropery { get; set; }
+        }
+    }
+}
---
+++ SigilProject/SampleClass.cs
@@ -0,0 +1,37 @@
+﻿using Sigil;
+using System;
+using System.Reflection;
+
+namespace SigilProject
+{
+    public class SampleClass
+    {
+        private static readonly Type maybeInternalClassType =
+               Type.GetType("CSharpProject.ClassWithPrivatePropertySample, CSharpProject");
+
+        private static readonly PropertyInfo maybeInternalProperty =
+            maybeInternalClassType.GetProperty("PrivatePropery", BindingFlags.Instance | BindingFlags.NonPublic);
+
+
+        private static readonly Emit<Func<object, string>> getProperyErmitterSample =
+        Emit<Func<object, string>>.NewDynamicMethod("GetPrivatePropertyValue")
+        .LoadArgument(0)
+        .CastClass(maybeInternalClassType)
+            .Call(maybeInternalProperty.GetGetMethod(true)!)
+            .Return();
+
+        private static readonly Func<object, string> GetPropertyErmittedDelegate =
+            getProperyErmitterSample.CreateDelegate();
+
+        public void CallPrivateProperySample()
+        {
+            var privateClassInstance = Activator.CreateInstance(maybeInternalClassType);
+            var result = GetPropertyErmittedDelegate(privateClassInstance);
+        }
+
+        class ClassWithPrivatePropertySample
+        {
+            private string PrivatePropery { get; set; }
+        }
+    }
+}
