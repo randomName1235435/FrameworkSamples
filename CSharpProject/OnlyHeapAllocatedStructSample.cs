a73fd4d6fcc3385066f7dde76db4b0aa086a8c4a
mm <unam3d@gmx.de>
2021-10-15 21:24:38 +0200

    added onlyHeapAllocatedStructSample

---
+++ CSharpProject/OnlyHeapAllocatedStructSample.cs
@@ -0,0 +1,19 @@
+﻿
+namespace CSharpProject
+{
+    public ref struct OnlyHeapAllocatedStructSample
+    {
+        public int SampleProperty { get; set; }
+    }
+    public class MyClass
+    {
+        public void Samplemethod()
+        {
+            var heapAllocatedStruct = new OnlyHeapAllocatedStructSample
+            {
+                SampleProperty = 0
+            };
+        }
+
+    }
+}
