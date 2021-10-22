b496d5c5bded547e040107ba446911c6c0b1d69a
mm <unam3d@gmx.de>
2021-10-22 08:01:23 +0200

    added ModulInitializerSample

---
+++ CSharpProject/ModulInitializerSample.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace CSharpProject
+{
+   public class ModulInitializerSample
+    {
+        [ModuleInitializer]
+        public static void GetCalledEvenBeforeMain() { }
+    }
+}
