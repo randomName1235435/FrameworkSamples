af6170c06912b13488cf3c57c8fe275a7b8dc852
mm <unam3d@gmx.de>
2021-10-22 09:46:57 +0200

    added record samples

---
+++ CSharpProject/RecordSample.cs
@@ -0,0 +1,12 @@
+﻿namespace CSharpProject
+{
+    class RecordSample
+    {
+        public void SampleMethod()
+        {
+            var sample = new SampleRecord("Sample", 1);
+            var sampleWith = sample with { SampleIntProperty = 2 };
+        }
+    }
+    internal record SampleRecord(string SampleStringProperty, int SampleIntProperty);
+}
