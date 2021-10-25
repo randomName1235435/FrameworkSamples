e1d73f951409a448de225fe40dd1f97eac5b487b
mm <unam3d@gmx.de>
2021-10-25 22:33:46 +0200

    added protobuf sample

---
+++ Protobuf/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using ProtoBuf;
+
+namespace Protobuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var fileStream = File.OpenRead(@"C:\"))
+            {
+                var deserialized = Serializer.Deserialize<IEnumerable<SampleClass>>(fileStream);
+            }
+            using (var fileStream = File.OpenWrite(@"C:\"))
+            {
+                Serializer.Serialize<IEnumerable<SampleClass>>(fileStream, new SampleClass[] { new SampleClass() });
+            }
+        }
+    }
+
+    [ProtoContract]
+    public class SampleClass
+    {
+        [ProtoMember(1)]
+        public int FirstSampleProperty { get; set; }
+
+        [ProtoMember(2)]
+        public int SecondSampleProperty { get; set; }
+    }
+}
