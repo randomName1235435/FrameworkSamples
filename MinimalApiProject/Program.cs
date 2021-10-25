94bf612d9ced5bcd5a81cf7f8dd4b7e006763786
mm <unam3d@gmx.de>
2021-10-25 22:04:21 +0200

    added more sample code

--- MinimalApiProject/Program.cs
+++ MinimalApiProject/Program.cs
@@ -1,3 +1,13 @@
 ﻿var app = WebApplication.CreateBuilder(args).Build();
 app.MapGet("/", () => "SampleMessage");
+app.MapGet("SampleString", () => "Sample");
+app.MapGet("Sample", () => new Sample("sample", 0));
 app.Run();
+
+
+//alternativ
+//var app = WebApplication.Create();
+//app.Run();
+
+record Sample(string FirstSampleProperty, int SecondSampleProperty);
+
