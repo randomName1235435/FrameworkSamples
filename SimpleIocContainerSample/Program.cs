9a763c43427cb5788f32fc86d1e256132fabdf5e
mm <unam3d@gmx.de>
2021-10-31 12:23:51 +0100

    added SimpleIocContainerSample added SimpleIocContainerSample/NoRegistrationsSample

---
+++ SimpleIocContainerSample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace SimpleIocContainerSample
+{
+    public class NoRegistrationsSample
+    {
+        static void Main(string[] args)
+        {
+            var installerImplementations =
+            typeof(NoRegistrationsSample).Assembly.ExportedTypes.Where(x =>
+                typeof(ISample).IsAssignableFrom(x)
+                && !x.IsInterface
+                && !x.IsAbstract)
+                .Select(Activator.CreateInstance)
+                .Cast<ISample>()
+                .ToList();
+
+            installerImplementations.ForEach(x => x.Sample());
+        }
+    }
+    public interface ISample {
+        void Sample();
+    }
+}
