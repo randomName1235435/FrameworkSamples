5202e26314dde9878f78ef71257a97977dcde2f5
mm <unam3d@gmx.de>
2021-11-07 19:04:10 +0100

    added SwaggerSample

---
+++ SwaggerSample/Startup.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace SwaggerSample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new OpenApiInfo { Title = "My Api", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            SwaggerConfiguration swaggerConfiguration = new SwaggerConfiguration();
+            Configuration.GetSection(nameof(SwaggerConfiguration)).Bind(swaggerConfiguration);
+
+            app.UseSwagger(o =>
+            {
+                o.RouteTemplate = swaggerConfiguration.JsonRoute;
+            });
+
+            app.UseSwaggerUI(o =>
+            {
+                o.SwaggerEndpoint(swaggerConfiguration.UIEndpoint, swaggerConfiguration.Description);
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+    public class SwaggerConfiguration
+    {
+        public string JsonRoute { get; set; }
+        public string Description { get; set; }
+        public string UIEndpoint { get; set; }
+    }
+}
