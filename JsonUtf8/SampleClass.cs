61174d291bd84aa18fdae4ce181d61442a72ccee
mm <unam3d@gmx.de>
2021-11-10 12:39:19 +0100

    added json samples

---
+++ JsonUtf8/SampleClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace JsonUtf8
+{
+    class SampleClass
+    {
+        static void Main(string[] args)
+        {
+            string jsonText = "";
+            Utf8Json.JsonSerializer.ToJsonString(new SampleClass());
+            Utf8Json.JsonSerializer.Deserialize<SampleClass>(jsonText);
+        }
+    }
+}
---
+++ SystemText/SampleClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SystemText
+{
+    class SampleClass
+    {
+        static void Main(string[] args)
+        {
+            string jsonText = "";
+            System.Text.Json.JsonSerializer.Serialize(new SampleClass());
+            System.Text.Json.JsonSerializer.Deserialize<SampleClass>(jsonText);
+        }
+    }
+}
