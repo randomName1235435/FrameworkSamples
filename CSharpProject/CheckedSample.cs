d995aa061266c15873b8de0082abf4da63479b2a
mm <unam3d@gmx.de>
2021-11-21 18:50:44 +0100

    CheckedSample

---
+++ CSharpProject/CheckedSample.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProject
+{
+    class CheckedSample
+    {
+        void SampleMethod() {
+
+            int sampleInt = int.MaxValue;
+
+            checked
+            {
+                sampleInt++; //throws exception
+            }
+
+            sampleInt++; // throws no exception but i will be 0
+
+        } 
+    }
+}
