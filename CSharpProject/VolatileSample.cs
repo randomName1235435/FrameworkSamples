4fe4a958dddd4104f79dfb94495b151f32d3a80b
mm <unam3d@gmx.de>
2021-11-21 19:50:36 +0100

    added VolatileSample

---
+++ CSharpProject/VolatileSample.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharpProject
+{
+    class VolatileSample
+    {
+        void SampleMethod() {
+
+            var sample = new Sample();
+            var sampleThread = new Thread(() => sample.SampleMethod());
+
+            sampleThread.Start();
+
+            while (sampleThread.IsAlive)
+            {
+            }
+            sample.PleaseStop();
+        }
+    }
+    class Sample
+    {
+        volatile bool shouldStop = false;
+
+        internal void SampleMethod() {
+            while (!shouldStop)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+        internal void PleaseStop()
+        {
+            shouldStop = true;
+        }
+    }
+}
