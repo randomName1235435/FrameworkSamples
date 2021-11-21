e4e4b71467b9904f42e6843507fceb573ff2b242
mm <unam3d@gmx.de>
2021-11-21 17:52:47 +0100

    added HarmonyProject

---
+++ HarmonyProject/Sample.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using System;
+
+namespace HarmonyProject
+{
+    [HarmonyPatch]
+    class Sample
+    {
+        [HarmonyPatch(typeof(Sample), "SampleMethod")] 
+        static void SampleAfterMethod() { }
+
+        static void SampleInitMethod()
+        {
+            var harmony = new Harmony("com.sample.patch");
+            harmony.PatchAll();
+        }
+
+        public void SampleMethod() { }
+    }
+}
