d254878687c9ae69ba974b844dad010d17a24ea0
mm <unam3d@gmx.de>
2021-11-21 19:41:11 +0100

    added GlobalSample

---
+++ CSharpProject/GlobalSample.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProject
+{
+    class GlobalSample
+    {
+        void SampleMethod() {
+            global::System.Console.WriteLine();
+        }
+    }
+    class Console
+    {
+
+    }
+}
