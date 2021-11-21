ee0c179580da55e396eed62b2d0ed35a5d6fff05
mm <unam3d@gmx.de>
2021-11-21 18:54:00 +0100

    added UnsafeSample

---
+++ CSharpProject/UnsafeSample.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProject
+{
+    class UnsafeSample
+    {
+        unsafe void SampleMethod() {
+
+            int sampleInteger = 0;
+
+            unsafe
+            {
+                int* samplePointer = &sampleInteger;
+                *samplePointer++;
+            }
+        }
+    }
+}
