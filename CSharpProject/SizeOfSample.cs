94ce90b1b3b9bfd9273f2f667bb98a9f371f2a62
mm <unam3d@gmx.de>
2021-11-21 19:08:39 +0100

    added SizeOfSample

---
+++ CSharpProject/SizeOfSample.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSharpProject
+{
+    class SizeOfSample
+    {
+        void SampleMethod() {
+            Console.WriteLine(sizeof(byte));
+            Size<byte>();
+            Size<SampleStruct>();
+        }
+        public static unsafe void Size<T>() where T : unmanaged {
+            Console.WriteLine(sizeof(T));
+        }
+    }
+    struct SampleStruct {
+
+        public int SampleProperty { get; init; }
+        public bool SeconSampleProperty { get; init; }
+    }
+}
