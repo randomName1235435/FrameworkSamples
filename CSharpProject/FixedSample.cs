88211e490d3c832c0c2d86f51b35ea79c5fe696b
mm <unam3d@gmx.de>
2021-11-21 19:02:01 +0100

    added FixedSample

---
+++ CSharpProject/FixedSample.cs
@@ -0,0 +1,14 @@
+﻿namespace CSharpProject
+{
+    class FixedSample
+    {
+        unsafe void SampleMethod()
+        {
+            string moveAbleSample = "123";
+            //fixes a movable allocation, so that the GC dont realloc the variable
+            fixed (char* fixedSample = moveAbleSample) { 
+            
+            }   
+        }
+    }
+}
