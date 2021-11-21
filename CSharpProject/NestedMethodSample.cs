7746875f743f48a03eaa31c59cab75b7a93931f4
mm <unam3d@gmx.de>
2021-11-21 17:24:06 +0100

    added nested method sample

---
+++ CSharpProject/NestedMethodSample.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProject
+{
+    class NestedMethodSample
+    {
+        public void SampleMethod()
+        {
+
+            SampleNestedMethod(0);
+
+            void SampleNestedMethod(int sampelParamether)
+            {
+            }
+        }
+    }
+}
