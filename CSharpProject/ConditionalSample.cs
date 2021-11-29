99a86d63b731cefbcc19ba274a8003ab64646d4a
mm <unam3d@gmx.de>
2021-11-29 20:55:02 +0100

    added  ConditionalSample

---
+++ CSharpProject/ConditionalSample.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+namespace CSharpProject
+{
+    class ConditionalSample
+    {
+        //note: methods are compiled but compiler will ignore method call if not compiled with specific config
+        [Conditional("DEBUG")]
+        private void SampleDebug() { }
+
+        [Conditional("RELEASE")]
+        private void SampleRelease() { }
+    }
+}
