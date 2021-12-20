eb9cd296e4e97752bf7a7703ce866aad93fdf3db
mm <unam3d@gmx.de>
2021-12-20 11:51:54 +0100

    added DeconstructorSample

---
+++ CSharpProject/DeconstructorSample.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProject
+{
+    class DeconstructorSample
+    {
+        public static void Sample() {
+
+            var dictionary = new Dictionary<int, int>() { { 1, 1 } };
+            var (key, value) = dictionary.First();
+
+            var sample = new DeconstructorSample(1,1);
+            var (prop1, prop2) = sample;
+        }
+
+        public DeconstructorSample(int sampleInt1, int sampleInt2)
+        {
+            SampleInt1 = sampleInt1;
+            SampleInt2 = sampleInt2;
+        }
+
+        public int SampleInt1 { get; }
+        public int SampleInt2 { get; }
+
+        public void Deconstruct(out int sampleProp1, out int sampleProp2) {
+            sampleProp1 = SampleInt1;
+            sampleProp2 = SampleInt2;
+        }
+    }
+}
