6a1a4e51aca99e3a910cf24b093f2b4119596f31
mm <unam3d@gmx.de>
2021-12-24 16:54:59 +0100

    added ActionOnDisposeSample

---
+++ CSharpProject/ActionOnDisposeSample.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProject
+{
+    class ActionOnDisposeSample
+    {
+        public void Sample()
+        {
+            using (new ActionOnDispose(() => { /*do stuff*/ }))
+            {
+
+            }
+        }
+    }
+    public class ActionOnDispose : IDisposable
+    {
+        private readonly Action actionOnDispose;
+
+        public ActionOnDispose(Action actionOnDispose)
+        {
+            this.actionOnDispose = actionOnDispose;
+        }
+        public void Dispose()
+        {
+            this.actionOnDispose();
+        }
+    }
+}
