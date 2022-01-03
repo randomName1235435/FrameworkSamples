2a0978582f398ee840d5110d40a21bb41bac39d0
mm <unam3d@gmx.de>
2022-01-03 22:16:11 +0100

    added dispose

---
+++ CSharpProject/CancelTokenSample.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharpProject
+{
+    class CancelTokenSample
+    {
+        public async void Sample()
+        {
+            var source = new CancellationTokenSource(1000);
+            var token = source.Token;
+            var result = await SampleMethod(token);
+            source.Cancel();
+            source.CancelAfter(1000);
+            source.Dispose();
+        }
+
+        private Task<int> SampleMethod(CancellationToken token)
+        {
+            return Task.Run(new Func<CancellationToken, int>(token =>
+                           {
+                               int count = 0;
+                               while (!token.IsCancellationRequested)
+                               {
+                                   try
+                                   {
+                                       Thread.Sleep(1000);
+                                       count++;
+                                   }
+                                   catch (TaskCanceledException e)
+                                   {
+                                       return count;
+                                   }
+                               }
+                               return 0;
+                           }),);
+
+        }
+    }
+}
