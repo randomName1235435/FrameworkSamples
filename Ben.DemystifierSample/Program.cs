a3f9d692cfe7d307787a98e171630bf07f2bf977
mm <unam3d@gmx.de>
2022-01-03 19:21:43 +0100

    added Ben.DemystifierSample

---
+++ Ben.DemystifierSample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Ben.DemystifierSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new ArgumentException("args");
+
+            }
+            catch (Exception x)
+            {
+
+                Console.WriteLine(x.StackTrace);
+                Console.WriteLine(x.Demystify());
+                Console.ReadLine();
+            }
+        }
+    }
+}
