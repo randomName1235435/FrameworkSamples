292cc16d013573e1025c04cd35d0027670fe7267
mm <unam3d@gmx.de>
2022-04-02 12:30:48 +0200

    added cocona sample

---
+++ CoconaSample/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ Simple standalone sample:
+CoconaApp.Run((string path, int length) =>
+{
+	Console.WriteLine(path + length);
+});
+ */
+
+using Cocona;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = CoconaApp.CreateBuilder();
+builder.Services.AddSingleton<ServiceToInjectSample>();
+var app = builder.Build();
+app.AddCommand("me", (string path, int length) =>
+{
+    Console.WriteLine(path + length);
+});
+app.AddCommand("sample", (ServiceToInjectSample service) =>
+{
+    service.DoSomething();
+    Console.WriteLine(service.GetTextMessage());
+});
+//calling: dotnet run sample
+//calling: dotnet run me
+app.Run();
+
+public class ServiceToInjectSample
+{
+    public void DoSomething() { }
+    public string GetTextMessage() { return string.Empty; }
+}
