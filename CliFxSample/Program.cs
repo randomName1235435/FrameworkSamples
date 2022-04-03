264c7c1684de36140a1c2bb3e01b82931f841db1
mm <unam3d@gmx.de>
2022-04-03 12:37:15 +0200

    added clifx sample more samples

--- CliFxSample/Program.cs
+++ CliFxSample/Program.cs
@@ -1,3 +1,26 @@
 ﻿using CliFx;
+using CliFxSample;
+using Microsoft.Extensions.DependencyInjection;
 
 static async Task Main(string[] args) => await new CliApplicationBuilder().AddCommandsFromThisAssembly().Build().RunAsync(args);
+
+
+static async Task<int> MainServiceRegistration()
+{
+    var services = new ServiceCollection();
+
+    // Register services
+    services.AddSingleton<SampleService>();
+
+    // Register commands
+    services.AddTransient<SampleCommand>();
+
+    var serviceProvider = services.BuildServiceProvider();
+
+    return await new CliApplicationBuilder()
+        .AddCommandsFromThisAssembly()
+        .UseTypeActivator(serviceProvider.GetService)
+        .Build()
+        .RunAsync();
+}
+
--- CliFxSample/SampleCommand.cs
+++ CliFxSample/SampleCommand.cs
@@ -1,21 +1,26 @@
 ﻿using CliFx;
 using CliFx.Attributes;
 using CliFx.Infrastructure;
+using System.Text;
 
 namespace CliFxSample
 {
     [Command]
     internal class SampleCommand : ICommand
     {
-        [CommandParameter(0, Description = "required parameter",IsRequired =true)]
+        [CommandParameter(0, Description = "required parameter", IsRequired = true)]
         public string SampleParameter { get; set; }
 
         [CommandOption('o', Description = "OutputParameter", IsRequired = false)]
         public bool OutputParameter { get; set; } = true;
 
+        [CommandOption("token", IsRequired = true, EnvironmentVariable = "AUTH_TOKEN")]
+        public string EnvironmentVariablesSample { get; init; }
+
         public async ValueTask ExecuteAsync(IConsole console)
         {
-            await console.Output.WriteLineAsync(SampleParameter);
+            CancellationToken cancellation = console.RegisterCancellationHandler();
+            await console.Output.WriteLineAsync(new StringBuilder(SampleParameter), cancellation);
         }
     }
 }
