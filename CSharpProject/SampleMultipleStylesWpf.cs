66dbe2b3187287b2bdbc5591e21b20b03276f317
mm <unam3d@gmx.de>
2022-04-18 18:01:12 +0200

    added SampleMultipleStylesWpf for changing ressource dictionaries at run time

---
+++ CSharpProject/SampleMultipleStylesWpf.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProject
+{
+    internal class SampleMultipleStylesWpf
+    {
+        public void ChangeStyle(string ressourceDictionaryName) {
+
+            //*notes: only works with complete independent ressource dictionaries, so use resmerger or something and dynamicResource in views/controls
+             
+            var ressources = new ResourceDictionary();
+            ressources.Source = new Uri(@"Resources/Dictionaries/" + path, UriKind.Relative);
+
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.Source = new Uri(@"Resources/Dictionaries/" + path, UriKind.Relative);
+        }
+    }
+}
