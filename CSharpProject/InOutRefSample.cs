32dd3c923af02b265df7da0fc6b2eadb23aed40c
mm <unam3d@gmx.de>
2022-04-18 18:47:35 +0200

    added InOutRefSample

---
+++ CSharpProject/InOutRefSample.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProject
+{
+    internal class InOutRefSample
+    {
+        public void Usage() {
+
+            int param = 0;
+
+            RefSample(ref param);
+        }
+
+        public void RefSample(ref int param) {
+            // by ref
+            // can modify param
+
+            param = 1;
+        }
+
+        public void InSample(in int param)
+        {
+            // by ref
+            // can not (hidden [readonly] attribut on param) modify param
+
+            param = 1; // wont compile
+        }
+
+        public void OutSample(out int param)
+        {
+            // by ref
+            // must modify/set param
+
+            param = 1; // wont compile if not set
+        }
+    }
+}
