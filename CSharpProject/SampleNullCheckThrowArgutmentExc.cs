a1a9250d01713a3891d02479ba408a3c908b03d5
mm <unam3d@gmx.de>
2022-04-18 21:30:42 +0200

    added SampleNullCheckThrowArgutmentExc

---
+++ CSharpProject/SampleNullCheckThrowArgutmentExc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProject
+{
+    public class SampleNullCheckThrowArgutmentExc
+    {
+        public int Sample(int? param) {
+
+            _ = param ?? throw new ArgumentNullException(nameof(param));
+
+            return param.Value;
+        }
+    }
+}
