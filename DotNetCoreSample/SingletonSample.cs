2c511d891b4dff0166e091c330297fd5dd21e1f6
mm <unam3d@gmx.de>
2022-07-31 14:28:49 +0200

    added dotnetcoresample added singletonsample

---
+++ DotNetCoreSample/SingletonSample.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DotNetCoreSample
+{
+    public class SingletonSample
+    {
+        public static Lazy<SingletonSample> lazySingletonInstance = new Lazy<SingletonSample>(() => new SingletonSample());
+        private SingletonSample() { }
+        public static SingletonSample GetInstance
+        {
+            get
+            {
+                return lazySingletonInstance.Value;
+            }
+        }
+    }
+}
