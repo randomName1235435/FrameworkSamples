cf0b6b02157a0671ad955c46aa43b90b5a8b46d2
mm <unam3d@gmx.de>
2022-07-31 15:51:37 +0200

    added DataContractBuiltInSample, IsolatedStorageSample

---
+++ CSharpProject/IsolatedStorageSample.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProject
+{
+    // file usage for silverlight, clickonce ..
+    internal class IsolatedStorageSample
+    {
+
+        public void WriteSample()
+        {
+            using (IsolatedStorageFile f = IsolatedStorageFile.GetMachineStoreForDomain())
+            using (var s = new IsolatedStorageFileStream("sample.txt", FileMode.Create, f))
+            using (var writer = new StreamWriter(s))
+                writer.WriteLine("sampleString");
+        }
+        public void ReadSample()
+        {
+            // Read it back:
+            using (IsolatedStorageFile f = IsolatedStorageFile.GetMachineStoreForDomain())
+            using (var s = new IsolatedStorageFileStream("sample.txt", FileMode.Open, f))
+            using (var reader = new StreamReader(s))
+                Console.WriteLine(reader.ReadToEnd()); //sampleString 
+        }
+    }
+}
---
+++ DataContractBuiltInSample/Sample.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.Serialization;
+
+namespace DataContractBuiltInSample
+{
+    public class Sample
+    {
+        public void Serialize()
+        {
+            var sample = new SampleClassToSerialize { SampleProp = 1 };
+            var ds = new DataContractSerializer(typeof(SampleClassToSerialize));
+            using (Stream stream = File.Create("sample.xml"))
+                ds.WriteObject(stream, sample);
+        }
+
+        public void Deserialize()
+        {
+            SampleClassToSerialize sample;
+            var ds = new DataContractSerializer(typeof(SampleClassToSerialize));
+
+            using (Stream stream = File.OpenRead("sample.xml"))
+                sample = (SampleClassToSerialize)ds.ReadObject(stream);
+
+        }
+
+        [DataContract]
+        public class SampleClassToSerialize
+        {
+            [DataMember] public int SampleProp { get; set; }
+        }
+    }
+}
