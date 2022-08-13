9e963325552c5872cf2cab1f4617f948c85f0f50
mm <unam3d@gmx.de>
2022-08-13 10:18:20 +0200

    added AsyncThreadSafeSample

---
+++ CSharpProject/AsyncThreadSafe.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharpProject
+{
+    internal class AsyncThreadSafeSample
+    {
+        private SemaphoreSlim semaphore = new SemaphoreSlim(1); //lockinstance
+
+        private async Task SampleMethodAsync()
+        {
+            await semaphore.WaitAsync();
+            try
+            {
+                await OtherSampleMethodeAsync();
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+        }
+
+        private async Task OtherSampleMethodeAsync()
+        {
+            await Task.Run(() => Thread.Sleep(1000));
+        }
+    }
+}
