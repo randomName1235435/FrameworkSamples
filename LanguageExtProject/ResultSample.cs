bbdb2b0d69f43b9aec56ccd169ec486499766fcf
mm <unam3d@gmx.de>
2022-08-13 19:58:45 +0200

    added ResultSample

---
+++ LanguageExtProject/ResultSample.cs
@@ -0,0 +1,29 @@
+﻿using LanguageExt.Common;
+
+namespace LanguageExtProject
+{
+    internal class ResultSample
+    {
+        private Result<bool> Sample()
+        {
+
+            var sampleException = new Exception();
+
+            return new Result<bool>(sampleException);
+        }
+
+        private bool SampleMatch()
+        {
+            return Sample().Match<bool>(r =>
+            {
+                return r;
+            },
+            e =>
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+            );
+        }
+    }
+}
