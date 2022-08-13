1ce7341bdad4b069020bb0f79190870284f72fc8
mm <unam3d@gmx.de>
2022-08-13 11:36:45 +0200

    added IndexSample

---
+++ CSharpProject/List.Array.Linq/IndexSample.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpProject.List.Array.Linq
+{
+    internal class IndexSample
+    {
+        private void SampleMethod()
+        {
+            var indexFromStart = new Index(0);
+            var indexFromEnd = new Index(0, true);
+            var sampleList = new List<int>() { 1, 2, 3 };
+            var item1 = sampleList[indexFromStart];
+            var item2 = sampleList[indexFromEnd];
+
+            var item3 = sampleList[^4]; // 4th from end
+
+            var range = sampleList.ToArray()[2..2];
+        }
+    }
+}
