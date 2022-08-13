5d8bd7dbfbdbf4ce43e520ae71c7b5fa7d0f1c84
mm <unam3d@gmx.de>
2022-08-13 14:39:45 +0200

    added OptionExtensionSample

---
+++ CSharpProject/Snippets/OptionExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+
+namespace CSharpProject.Snippets
+{
+    public static class OptionExtensionSample
+    {
+        public static void AddSample(this IServiceCollection serviceCollection, Action<OptionSample>? options = null)
+        {
+            serviceCollection.Configure(options);
+        }
+
+        public class OptionSample
+        {
+            public int Sample { get; set; } = 0;
+        }
+    }
+}
