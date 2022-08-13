1637cd6ba364a32c2d90e17716342ade81e403c4
mm <unam3d@gmx.de>
2022-08-13 10:25:27 +0200

    added HumanizerProject

---
+++ HumanizerProject/Sample.cs
@@ -0,0 +1,20 @@
+﻿using Humanizer;
+
+namespace HumanizerProject
+{
+    public class Sample
+    {
+        private void SampleMethod()
+        {
+            var filesize = (10).Kilobytes();
+            var bist = filesize.Bits;
+            var bytes = filesize.Bytes;
+            var kilobytes = filesize.Kilobytes;
+            var megabytes = filesize.Megabytes;
+            var gigabytes = filesize.Gigabytes;
+            var terabytes = filesize.Terabytes;
+
+            var twoTimes = kilobytes + terabytes;
+        }
+    }
+}
