e46271a836e826f06ee98a5a79b7c22642327b51
mm <unam3d@gmx.de>
2022-08-13 13:53:34 +0200

    added PeriodicTimerSample

---
+++ CSharpProject/Snippets/TimerSample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharpProject.Snippets
+{
+    internal class PeriodicTimerSample
+    {
+        private async Task Sample(CancellationToken cancellationToken)
+        {
+            var periodicTimer = new PeriodicTimer(TimeSpan.FromSeconds(1));
+            while (await periodicTimer.WaitForNextTickAsync(cancellationToken) && !cancellationToken.IsCancellationRequested)
+            {
+                await DoSOmethingAsync();
+            }
+        }
+
+        private Task DoSOmethingAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
