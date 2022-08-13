1c824ea66a36258b4ee0a32ed8494325cdac6f4a
mm <unam3d@gmx.de>
2022-08-13 11:21:03 +0200

    added OperatorSample

---
+++ CSharpProject/Keywords/RangeOperatorSample.cs
@@ -0,0 +1,74 @@
+﻿namespace CSharpProject.Keywords
+{
+    internal class OperatorSample
+    {
+
+        private void BinarySample()
+        {
+            var sampleOne = 0;
+            var sampleTwo = 1;
+            var result = 0;
+
+            result = sampleOne + sampleTwo;
+            result = sampleOne - sampleTwo;
+            result = sampleOne * sampleTwo;
+            result = sampleOne / sampleTwo;
+            result = sampleOne % sampleTwo;
+            result = sampleOne & sampleTwo;
+            result = sampleOne | sampleTwo;
+            result = sampleOne << sampleTwo;
+            result = sampleOne >> sampleTwo;
+        }
+
+        private void UnarySample()
+        {
+            var sampleOne = 0;
+            var sampleTwo = 1;
+            var result = 0;
+
+            result = sampleOne!;
+            result = ~sampleOne;
+            result = sampleOne++;
+            result = sampleOne--;
+            result = sampleOne;
+        }
+
+        private void RelationalSample()
+        {
+            var sampleOne = 0;
+            var sampleTwo = 1;
+            var result = false;
+
+            result = sampleOne == sampleTwo;
+            result = sampleOne != sampleTwo;
+            result = sampleOne < sampleTwo;
+            result = sampleOne > sampleTwo;
+            result = sampleOne <= sampleTwo;
+            result = sampleOne >= sampleTwo;
+        }
+
+        private void ConcatConversionSample()
+        {
+            bool sampleOne = false;
+            bool sampleTwo = true;
+            var result = false;
+
+            result = (bool)sampleOne;
+            result = sampleOne && sampleTwo;
+            result = sampleOne || sampleTwo;
+        }
+
+        private void CompundAssignmenntSample()
+        {
+            var sampleOne = 0;
+            var sampleTwo = 1;
+            var result = 0;
+
+            result += sampleOne;
+            result -= sampleOne;
+            result *= sampleOne;
+            result /= sampleOne;
+            result %= sampleOne;
+        }
+    }
+}
