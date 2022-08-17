801208b78ec392c59e0c8ebbf1a039b1f52cbeb0
mm <unam3d@gmx.de>
2022-08-17 10:39:41 +0200

    added LegitAsycVoidSample

---
+++ CSharpProject/Async.Task.Threading/LegitAsycVoidSample.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+
+namespace CSharpProject.Async.Task.Threading
+{
+    internal class LegitAsycVoidSample
+    {
+        public LegitAsycVoidSample()
+        {
+            SampleAsycTaskMethod(); // missing await :(
+            CallSampleAsycTaskMethod();
+        }
+
+        private async void CallSampleAsycTaskMethod()
+        {
+            await SampleAsycTaskMethod(); //await happyface
+        }
+
+        private async Task<Sample> SampleAsycTaskMethod()
+        {
+            await System.Threading.Tasks.Task.Delay(0);
+            return null;
+        }
+
+        private class Sample { }
+    }
+
+}
