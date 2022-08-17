3e7a9c3efa3ed605d27996b26d36e2c2db92fc4f
mm <unam3d@gmx.de>
2022-08-18 00:05:15 +0200

    added ArrayPoolSample

---
+++ CSharpProject/List.Array.Linq/ArrayPoolSample.cs
@@ -0,0 +1,18 @@
+﻿using System.Buffers;
+
+namespace CSharpProject.List.Array.Linq
+{
+    internal class ArrayPoolSample
+    {
+        private void Sample()
+        {
+            var arrayPool = ArrayPool<byte>.Shared;
+            var buffer = arrayPool.Rent(1000);
+            //by default array is not cleared
+            using (new ActionOnDispose(() => { arrayPool.Return(buffer); }))
+            {
+                //do stuff with array
+            }
+        }
+    }
+}
