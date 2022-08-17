327e7026e0c4ce7fee77663405256dcf18649de9
mm <unam3d@gmx.de>
2022-08-17 21:57:58 +0200

    added SharedMemorySample

---
+++ CSharpProject/Snippets/SharedMemorySample.cs
@@ -0,0 +1,47 @@
+﻿using System.IO.MemoryMappedFiles;
+
+namespace CSharpProject.Snippets
+{
+    internal class SharedMemorySample
+    {
+        private void SampleWrite()
+        {
+            // shared memory can accessed by multiple processes
+            using (var memoryMappedFile = MemoryMappedFile.CreateNew("sharedMemory", 1000))
+            {
+                using (var viewAccessor = memoryMappedFile.CreateViewAccessor())
+                {
+                    viewAccessor.Write(500, 1000);
+                }
+            }
+        }
+        private void SampleRead()
+        {
+            // shared memory can accessed by multiple processes
+            using (var memoryMappedFile = MemoryMappedFile.OpenExisting("sharedMemory"))
+            {
+                using (var viewAccessor = memoryMappedFile.CreateViewAccessor())
+                {
+                    var sample = viewAccessor.ReadInt32(500);
+                }
+            }
+        }
+        private void SampleFastWrite()
+        {
+            unsafe
+            {
+                using (var memoryMappedFile = MemoryMappedFile.CreateNew("sharedMemory", 1000))
+                {
+                    using (var viewAccessor = memoryMappedFile.CreateViewAccessor())
+                    {
+                        byte* bytePointer = (byte*)viewAccessor.SafeMemoryMappedViewHandle.DangerousGetHandle().ToPointer();
+                        bytePointer += 500;
+                        int* intPointer = (int*)bytePointer;
+                        *intPointer = 1000;
+                    }
+                }
+            }
+
+        }
+    }
+}
