3a508d30b57b1c314ce3e0dd396c51d8db04413e
mm <unam3d@gmx.de>
2022-08-17 22:33:40 +0200

    added AssemblyLoadContextSample

---
+++ DotNetCoreSample/AssemblyLoadContextSample.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Runtime.Loader;
+
+namespace DotNetCoreSample
+{
+    internal class AssemblyLoadContextSample : AssemblyLoadContext
+    {
+        private void Sample() {
+            //replacement for appDomains
+            var context = new AssemblyLoadContextSample();
+            var jsonInContext = context.LoadFromAssemblyName(new System.Reflection.AssemblyName("Newtonsoft.Json"));
+
+            var jObjectType = jsonInContext.GetType(typeof(JObject).FullName);
+            var obj = Activator.CreateInstance(jObjectType);
+
+            var jObjInContext = (JObject)obj;
+        }
+    }
+}
