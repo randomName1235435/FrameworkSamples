5c5bf6c9e2de43317e9481151424c8961c09f398
mm <unam3d@gmx.de>
2022-08-18 11:45:56 +0200

    added CombinedAccessModifierSample

---
+++ CSharpProject/Keywords/CombinedAccessModifierSample.cs
@@ -0,0 +1,12 @@
+﻿namespace CSharpProject.Keywords
+{
+    internal class CombinedAccessModifierSample
+    {
+        protected internal CombinedAccessModifierSample()
+        {
+
+        }
+
+        protected internal virtual void Sample() { }
+    }
+}
