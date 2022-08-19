2a88aeb7e1e366f09a4254b00107c9f23e083754
mm <unam3d@gmx.de>
2022-08-19 17:02:35 +0200

    added ForeachWithoutIEnumeratorSample

---
+++ CSharpProject/Compiling.Reflection/ForeachWithoutIEnumeratorSample.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+
+namespace CSharpProject.Compiling.Reflection
+{
+    internal class ForeachWithoutIEnumeratorSample
+    {
+        private void Sample()
+        {
+            foreach (var item in this)
+            {
+                // works cause foreach works by convention
+                // if GetEnumerator method is available the specific interface not needed
+            }
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return null;
+        }
+    }
+}
