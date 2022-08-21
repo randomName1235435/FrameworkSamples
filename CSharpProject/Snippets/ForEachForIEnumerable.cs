608f2d907b35d86042c9fe1184080cf3abb7f683
mm <unam3d@gmx.de>
2022-08-21 17:09:01 +0200

    added SelectAsyncSample added ForEachForIEnumerable

---
+++ CSharpProject/Snippets/ForEachForIEnumerable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpProject.Snippets
+{
+    internal static class ForEachForIEnumerable
+    {
+        public static void ForEach<T>(this IEnumerable<T> enumeration, Action<T> action)
+        {
+            foreach (T item in enumeration)
+            {
+                action(item);
+            }
+        }
+    }
+}
---
+++ DotNetCoreSample/SelectAsyncSample.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreSample
+{
+    internal static class SelectAsyncSample
+    {
+        public static async Task<IEnumerable<TResult>> SelectAsync<TSource, TResult>(
+    this IEnumerable<TSource> source, Func<TSource, Task<TResult>> method)
+        {
+            return await Task.WhenAll(source.Select(async s => await method(s)));
+        }
+    }
+}
