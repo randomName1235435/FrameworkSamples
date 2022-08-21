7f32f363448edb7f69b2f50c1f2ca1175833cd44
mm <unam3d@gmx.de>
2022-08-21 12:26:34 +0200

    added LightInjectSample

---
+++ LightInjectSample/Sample.cs
@@ -0,0 +1,18 @@
+﻿namespace LightInjectSample
+{
+    using LightInject;
+    public class Sample
+    {
+        private void SampleMethod()
+        {
+            var container = new ServiceContainer();
+
+            container.Register<ISampleService, SampleService>();
+            container.GetInstance<ISampleService>();
+            var instance = container.GetInstance<ISampleService>();
+        }
+    }
+    public interface ISampleService { }
+    public class SampleService : ISampleService { }
+
+}
