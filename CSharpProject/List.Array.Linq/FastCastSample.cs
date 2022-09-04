8c4bba4822ddbd9ab0de1b35084fa7adea2de75d
mm <unam3d@gmx.de>
2022-09-04 22:44:49 +0200

    added fastcastsample

---
+++ CSharpProject/List.Array.Linq/FastCastSample.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharpProject.List.Array.Linq;
+
+public static class FastCastSample
+{
+    public static IEnumerable<ToCast> FastCast<ToCast>(this IEnumerable<object> collection)
+    {
+        return collection
+            .Where(item => item.GetType() == typeof(ToCast))
+            .Select(item => (ToCast)item);
+    }
+}
