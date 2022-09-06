ee5b0fe406c776af54ef3c2232241bca20746bb7
mm <unam3d@gmx.de>
2022-09-06 16:01:46 +0200

    added RespawnSample

---
+++ RespawnSample/SampleClass.cs
@@ -0,0 +1,38 @@
+﻿using System.Data.Common;
+using Respawn;
+using Respawn.Graph;
+
+namespace RespawnSample;
+
+public class SampleClass
+{
+    public async Task Sample()
+    {
+        var checkpoint = new Checkpoint
+        {
+            TablesToIgnore = new[]
+            {
+                "sysdiagrams",
+                "tblTest",
+                "tblObjectType",
+                new Table("import", "Task")
+            },
+            SchemasToExclude = new[]
+            {
+                "Sample"
+            },
+            DbAdapter = DbAdapter.SqlServer
+        };
+
+        await checkpoint.Reset("MyConnectionStringName");
+    }
+
+    public async Task UseConnectionSample(Func<DbConnection> connectionProvider)
+    {
+        Checkpoint checkpoint = null; // your checkpoint
+        await using var conn = connectionProvider();
+        await conn.OpenAsync();
+
+        await checkpoint.Reset(conn);
+    }
+}
