d3176ab10c53d4aa9d0426557e6e8653967fd901
mm <mm@mm.mm>
2023-04-09 14:14:01 +0200

    added debugging to compiling.reflection; added DebuggerDisplay

---
+++ CSharpProject/Compiling.Reflection.Debugging/DebuggerDisplaySample.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProject.Compiling.Reflection.Debugging
+{
+    [DebuggerDisplay("{sampleProperty}", Name = "{sampleName}")]
+    internal class DebuggerDisplaySample
+    {
+        private string sampleName = "";
+        public int sampleProperty=4;
+    }
+}
