5bbe8fe98fd7bb56a789c67f4863fe389bc017a6
mm <mm@mm.mm>
2024-02-28 21:38:15 +0100

    integrated ironpython project aka UsingPythonInDotNet

---
+++ IronPythonSample/Program.cs
@@ -0,0 +1,8 @@
+﻿using IronPython.Hosting;
+
+dynamic pyhthon = Python.CreateRuntime().UseFile("./multiplier.py");
+var multiplier = pyhthon.multiplier;
+
+int result = multiplier.calculate(10, 10);
+
+Console.WriteLine($"result {result}");
---
+++ IronPythonSample/multiplier.py
@@ -0,0 +1,6 @@
+﻿﻿class multiplier:
+	@staticmethod
+	def calculate(x,y): return x*y
+{
+}
+}
